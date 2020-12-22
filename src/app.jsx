46c0df148983200f99145ade3c3119920ed9cd6a
Hamin <haminjp@naver.com>
2020-12-23 06:29:25 +0900

    feat: footer

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import "./app.css";
+import styles from "./app.module.css";
 import Header from "./components/header/header";
 import TimerContainer from "./components/timer_container/timer_container";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Header />
       <TimerContainer />
+      <footer className={styles.footer}>
+        <a
+          href="https://github.com/peppermintc/peppermint-timer"
+          target="_blank"
+        >
+          <img src="/images/github.png" alt="github logo" />
+        </a>
+        <span class={styles.footer_text}>Developed by peppermint</span>
+      </footer>
     </div>
   );
 }
